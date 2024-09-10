db4abcac5d291a434395d51a17cb771804b5726e
sperezp23 <sperezp23@gmail.com>
2024-09-09 23:54:57 -0500

    Update: 'Markdown, datos promedio corrido'

--- Guardar_datos.py
+++ Guardar_datos.py
@@ -1,6 +1,6 @@
 datos_profe_max = pd.DataFrame()
-datos_profe_max['año'] = curva_de_luz_agrupada_df.obs_date.dt.year
-datos_profe_max['mes'] = curva_de_luz_agrupada_df.obs_date.dt.month
-datos_profe_max['día'] = curva_de_luz_agrupada_df.obs_date.dt.day
-datos_profe_max = pd.concat([datos_profe_max,curva_de_luz_agrupada_df[['magnitude', 'delta', 'r', 'magnitud_reducida']]], axis=1)
+datos_profe_max['año'] = curva_de_luz_externa_df.obs_date.dt.year
+datos_profe_max['mes'] = curva_de_luz_externa_df.obs_date.dt.month
+datos_profe_max['día'] = curva_de_luz_externa_df.obs_date.dt.day
+datos_profe_max = pd.concat([datos_profe_max,curva_de_luz_externa_df[['magnitude', 'delta', 'r', 'magnitud_reducida']]], axis=1)
 datos_profe_max.to_csv('Minimos_agrupados_por_dia.txt', sep='\t')
